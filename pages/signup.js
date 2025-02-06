e06e2b796e6b3caf56280134e27bdbb8a7f7a126
mobilacks <mobi.ahmed@lacksvalley.com>
2025-02-06 16:51:08 -0600

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,5 @@
 import AuthForm from "../components/AuthForm";
 
 export default function Signup() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <AuthForm isSignUp={true} />
-    </div>
-  );
+  return <AuthForm isSignUp={true} />;
 }
