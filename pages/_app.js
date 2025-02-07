ca4c287e8fd024559650fb09a31e03a8e6ca4cec
mobilacks <mobi.ahmed@lacksvalley.com>
2025-02-06 18:05:45 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,7 @@
-import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabase";
-import { useRouter } from "next/router";
-import "../styles/globals.css";
+import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }) {
-  const [session, setSession] = useState(null);
-  const router = useRouter();
-
-  useEffect(() => {
-    const fetchSession = async () => {
-      const { data: sessionData } = await supabase.auth.getSession();
-      setSession(sessionData?.session);
-    };
-
-    fetchSession();
-    
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-      if (session) router.push("/dashboard");
-    });
-
-    return () => listener?.subscription.unsubscribe();
-  }, []);
-
-  return (
-    <div className="font-roboto">
-      <Component {...pageProps} />
-    </div>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
