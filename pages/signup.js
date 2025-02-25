3b9b400800f19222143f537b6e0743728c0bf7b2
mobilacks <mobi.ahmed@lacksvalley.com>
2025-02-25 12:16:00 -0600

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,19 @@
 import AuthForm from "../components/AuthForm";
+import Image from "next/image";
 
 export default function Signup() {
-  return <AuthForm isSignUp={true} />;
+  return (
+    <div className="login-container">
+      <div className="auth-box">
+        {/* ✅ Add Logo */}
+        <Image src="/logo.png" alt="Lacks Furniture Logo" width={120} height={50} />
+        
+        {/* ✅ Add Header */}
+        <h2 className="login-header">Lacks UPS System</h2>
+
+        {/* ✅ Signup Form */}
+        <AuthForm isSignUp={true} />
+      </div>
+    </div>
+  );
 }
