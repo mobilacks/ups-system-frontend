d8d7ba273af57177385f3ed0630df575741d1c54
mobilacks <198098276+mobilacks@users.noreply.github.com>
2025-03-04 12:55:54 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import Head from "next/head"; // ✅ Import Head for metadata
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" /> {/* ✅ Favicon added */}
+      </Head>
       <Navbar /> {/* ✅ Navbar Included */}
       <Component {...pageProps} />
     </>
